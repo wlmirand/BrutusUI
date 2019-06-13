a0c0f9f867de73d66d0be1149964c09020e1c061
William Miranda <williamlopes.miranda@altran.com>
2019-06-13 11:37:37 +0100

    Adding and EditFloat
    Signed-off-by: William Miranda <williamlopes.miranda@altran.com>

--- BrutusUI/src/main/java/william/miranda/brutusui/BrutusUIEditFloat.kt
+++ BrutusUI/src/main/java/william/miranda/brutusui/BrutusUIEditFloat.kt
@@ -11,24 +11,24 @@ import william.miranda.brutusui.databinding.BrutusuiGenericBinding
 /**
  * Class to store Integer values
  */
-class BrutusUIEditInt(context: Context, attrs: AttributeSet) : BrutusUIGeneric<Int>(context, attrs) {
+class BrutusUIEditFloat(context: Context, attrs: AttributeSet) : BrutusUIGeneric<Float>(context, attrs) {
 
     /**
      * Render Function
      */
-    override var renderFunction: (Int?) -> String? = { it?.toString() }
+    override var renderFunction: (Float?) -> String? = { it?.toString() }
 
     init {
         //Inflate the Layout
         BrutusuiGenericBinding.inflate(LayoutInflater.from(context), this, true).run {
-            title = this@BrutusUIEditInt.title
-            summary = this@BrutusUIEditInt.summary
+            title = this@BrutusUIEditFloat.title
+            summary = this@BrutusUIEditFloat.summary
         }
 
         //Get the value from XML and set to the Field
         with(context.obtainStyledAttributes(attrs, R.styleable.BrutusUIGeneric)) {
             if (hasValue(R.styleable.BrutusUIGeneric_value)) {
-                value.set(getInt(R.styleable.BrutusUIGeneric_value, 0))
+                value.set(getFloat(R.styleable.BrutusUIGeneric_value, 0f))
             }
 
             recycle()
@@ -49,23 +49,25 @@ class BrutusUIEditInt(context: Context, attrs: AttributeSet) : BrutusUIGeneric<I
      * Mostra a dialog
      */
     private fun showDialog() {
-        val dialog = with(AlertDialog.Builder(context, androidx.appcompat.R.style.Base_ThemeOverlay_AppCompat_Dialog_Alert)) {
-            setTitle(title.get())
-            setView(R.layout.brutusui_dialog_edittext)
+        val dialog =
+            with(AlertDialog.Builder(context, androidx.appcompat.R.style.Base_ThemeOverlay_AppCompat_Dialog_Alert)) {
+                setTitle(title.get())
+                setView(R.layout.brutusui_dialog_edittext)
 
-            setPositiveButton(android.R.string.ok) { di, _ ->
-                //first get the String value
-                val strValue = (di as AlertDialog).findViewById<EditText>(R.id.dialog_edittext)?.text?.toString()
+                setPositiveButton(android.R.string.ok) { di, _ ->
+                    //first get the String value
+                    val strValue = (di as AlertDialog).findViewById<EditText>(R.id.dialog_edittext)?.text?.toString()
 
-                //Save the inputted value
-                value.set( strValue?.takeIf { strValue.isNotEmpty() }?.toInt() )
+                    //Save the inputted value
+                    value.set(strValue?.takeIf { strValue.isNotEmpty() }?.toFloat())
+                }
+                setNegativeButton(android.R.string.cancel, null)
+                show()
             }
-            setNegativeButton(android.R.string.cancel, null)
-            show()
-        }
 
         //Define the InputType
-        dialog.findViewById<EditText>(R.id.dialog_edittext)?.inputType = InputType.TYPE_CLASS_NUMBER
+        dialog.findViewById<EditText>(R.id.dialog_edittext)?.inputType =
+            InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL or InputType.TYPE_NUMBER_FLAG_SIGNED
 
         //If we have some value, put it in the Dialog
         value.get()?.let { dialog.findViewById<EditText>(R.id.dialog_edittext)?.setText(it.toString()) }
